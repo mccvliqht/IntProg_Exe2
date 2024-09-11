2847bb585efaaedf077454bbb017ce09d854c679
zeneralk28 <masuelaglen@gmail.com>
2024-09-11 09:32:35 +0800

    modify greeting php

--- greeting.php
+++ greeting.php
@@ -4,12 +4,12 @@ date_default_timezone_set('Asia/Manila'); // Set timezone
 $hour = date('H');
 
 if ($hour >= 5 && $hour < 12) {
-    echo "<h2>Good Morning!</h2>";
+    echo "<h2>Good Morning!</h2>".date("Y-m-d");
 } elseif ($hour >= 12 && $hour < 17) {
-    echo "<h2>Good Afternoon!</h2>";
+    echo "<h2>Good Afternoon!</h2>".date("Y-m-d");
 } elseif ($hour >= 17 && $hour < 21) {
-    echo "<h2>Good Evening!</h2>";
+    echo "<h2>Good Evening!</h2>".date("Y-m-d");
 } else {
-    echo "<h2>Good Night!</h2>";
+    echo "<h2>Good Night!</h2>".date("Y-m-d");
 }
 
